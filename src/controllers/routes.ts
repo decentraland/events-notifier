a45a0c8ea01219c61b55203c51a129528032758e
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-07-05 11:20:05 -0300

    feat: expose set-cursor endpoint

---
+++ src/controllers/routes.ts
@@ -0,0 +1,20 @@
+import { Router } from '@well-known-components/http-server'
+import { bearerTokenMiddleware, errorHandler } from '@dcl/platform-server-commons'
+import { GlobalContext } from '../types'
+import { setCursorHandler } from './handlers/set-cursor'
+
+// We return the entire router because it will be easier to test than a whole server
+export async function setupRouter({ components }: GlobalContext): Promise<Router<GlobalContext>> {
+  const router = new Router<GlobalContext>()
+
+  const { config } = components
+
+  const signingKey = await config.getString('SIGNING_KEY')
+  if (signingKey) {
+    router.post('/producers/:producer/set-since', bearerTokenMiddleware(signingKey), setCursorHandler)
+  }
+
+  router.use(errorHandler)
+
+  return router
+}
