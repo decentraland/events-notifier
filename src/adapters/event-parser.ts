66772c003e26edf8e735069147466fc258972e90
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-09-04 17:50:37 -0300

    feat: forward UsedEmote explorer event

--- src/adapters/event-parser.ts
+++ src/adapters/event-parser.ts
@@ -1,15 +1,18 @@
-import { Event, Events, MoveToParcelEvent } from '@dcl/schemas'
+import { Event, Events, MoveToParcelEvent, UsedEmoteEvent } from '@dcl/schemas'
 
 enum ExplorerEventIds {
-  MOVE_TO_PARCEL = 'move_to_parcel'
+  MOVE_TO_PARCEL = 'move_to_parcel',
+  USED_EMOTE = 'used_emote'
 }
 
 function parseExplorerClientEvent(event: any): Event | undefined {
-  if (event && (event.event as string).toLocaleLowerCase() === ExplorerEventIds.MOVE_TO_PARCEL) {
+  if (!event || !event.event) return undefined
+
+  if ((event.event as string).toLocaleLowerCase() === ExplorerEventIds.MOVE_TO_PARCEL) {
     return {
       type: Events.Type.CLIENT,
       subType: Events.SubType.Client.MOVE_TO_PARCEL,
-      timestamp: event.timestamp,
+      timestamp: new Date(event.timestamp).getTime(),
       key: event.messageId,
       metadata: {
         authChain: JSON.parse(event.context.auth_chain),
@@ -25,6 +28,28 @@ function parseExplorerClientEvent(event: any): Event | undefined {
         }
       }
     } as MoveToParcelEvent
+  }
+
+  if ((event.event as string).toLocaleLowerCase() === ExplorerEventIds.USED_EMOTE) {
+    return {
+      type: Events.Type.CLIENT,
+      subType: Events.SubType.Client.USED_EMOTE,
+      timestamp: new Date(event.timestamp).getTime(),
+      key: event.messageId,
+      metadata: {
+        authChain: JSON.parse(event.context.auth_chain),
+        userAddress: event.context.dcl_eth_address,
+        sessionId: event.context.session_id,
+        timestamp: event.sentAt,
+        realm: event.context.realm,
+        emote: {
+          emoteIndex: event.properties.emote_index,
+          isBase: event.properties.is_base,
+          itemId: event.properties.item_id,
+          source: event.properties.source
+        }
+      }
+    } as UsedEmoteEvent
   }
 
   return undefined
